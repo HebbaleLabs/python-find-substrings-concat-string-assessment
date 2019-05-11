5802136bf383fb6f241586095fc72b05cdd47750
Vineet Reynolds Pereira <Vineet.Reynolds@gmail.com>
2019-05-11 09:53:00 +0530

    Added tests for large input sequence size

--- tests/test_find_substrings.py
+++ tests/test_find_substrings.py
@@ -79,3 +79,33 @@ class FindSubstringsTest(unittest.TestCase):
     message = 'For input values: {0}, desired_concatenated_string = \'{1}\', expected value = {2}, and actual value = {3}'.format(
         strings, desired_concatenated_string, expected_found, actual_found)
     self.assertEqual(expected_found, actual_found, message)
+
+  @parameterized.expand([
+    ('list with large number of strings',
+     ['FQI', 'ZSR', 'YRU', 'SJZ', 'NOY', 'NCE', 'CEJ', 'GFN', 'ILC', 'DLX',
+      'BMS', 'YZS', 'CCG', 'LMJ', 'KNZ', 'LPL', 'OIB', 'ASS', 'CKT', 'VQY',
+      'FJC', 'VRZ', 'WGY', 'BRY', 'UVE', 'KTR', 'GRL', 'PZC', 'WIE', 'CDM',
+      'AHT', 'NXQ', 'YSS', 'SRL', 'WVR', 'BDB', 'HOK', 'EHY', 'JBO', 'UCW',
+      'QKU', 'CQD', 'OGQ', 'BTH', 'CJI', 'JQX', 'XWX', 'MCB', 'NWP', 'CZI'],
+     'CJIVRZ', True),
+    ('list with large number of strings',
+     ['9DP', 'MVQ', 'A9T', 'CRZ', 'CI8', 'QT1', 'D0F', 'SSX', 'PRK', 'LKH',
+      'YCW', 'VNU', 'D6F', 'JMX', 'ACT', '15E', '4QQ', 'QK3', 'R6E', '1A7',
+      '46X', 'M1M', 'KNJ', 'R3B', 'WAS', 'AN9', '2QN', '7M7', 'GMU', '5R8',
+      'FKG', 'KAG', 'XZR', '9BN', 'KMS', '1ZB', 'WXC', 'KH1', 'SIV', 'RLT',
+      'SDX', 'LGW', 'DU5', 'LDT', 'GQ7', 'JMM', 'CZN', 'GCU', 'HYT', '0UF',
+      'Q48', 'XDG', 'WOI', 'USX', 'VOU', '08W', 'QGJ', 'NDS', '67U', '9YU',
+      '1KO', 'Z58', 'JYM', 'JWX', 'A45', 'OV4', '8VG', 'UHV', '1AX', 'WBQ',
+      'GAV', 'QS0', 'Z99', 'MM7', 'J4T', 'XZK', 'NHR', '8K1', 'W8O', 'YC0',
+      'V57', 'NT7', 'ZMI', 'UKV', '485', 'P18', 'HVM', 'PSG', 'EE0', 'M1U',
+      'TFR', 'XQN', 'ZL6', 'T19', 'KDM', 'YBU', 'YC7', 'G0L', 'FYD', 'U1T'],
+     'LGW1DP', False)
+  ])
+  def test_find_concatenation_large_input_sequence(self, name, strings,
+      desired_concatenated_string,
+      expected_found):
+    actual_found = is_matching_concat_string_pair(strings,
+                                                  desired_concatenated_string)
+    message = 'For input values: {0}, desired_concatenated_string = \'{1}\', expected value = {2}, and actual value = {3}'.format(
+        strings, desired_concatenated_string, expected_found, actual_found)
+    self.assertEqual(expected_found, actual_found, message)
